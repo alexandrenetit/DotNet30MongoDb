8cabc16a7b8ea577d2bfaf2b7b87ee9958cfc002
Alexandre Gonçalves <alexandre.goncalves@farfetch.com>
2019-11-29 14:59:20 +0000

    Add

---
+++ BooksApi/BooksApi/Services/BookService.cs
@@ -0,0 +1,20 @@
+﻿using BooksApi.Models;
+using MongoDB.Driver;
+
+namespace BooksApi.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _book;
+
+        public BookService(IBookstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DataBaseName);
+
+            _book = database.GetCollection<Book>(settings.BooksCollectionName);
+        }
+
+
+    }
+}
